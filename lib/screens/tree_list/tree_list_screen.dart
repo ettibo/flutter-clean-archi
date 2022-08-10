4da58d0060a3559c783a89e89c3787a2998fa6ca
Dylan Hamiti <d.hamiti@groupeonepoint.com>
2022-08-10 14:30:30 +0200

    Improved the UI of the List in the tree_list_screen

--- lib/screens/tree_list/tree_list_screen.dart
+++ lib/screens/tree_list/tree_list_screen.dart
@@ -1,9 +1,12 @@
-import 'package:api/dependency_injection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:api/dependency_injection.dart';
+import 'package:api/models/app/tree/tree.dart';
 import 'package:globo_fitness/screens/tree_list/tree_list_view_model.dart';
 import 'package:globo_fitness/shared/menu_bottom.dart';
 import 'package:globo_fitness/shared/menu_drawer.dart';
+import 'package:globo_fitness/shared/circular_progress_indicator.dart';
+import 'package:globo_fitness/extensions/string_casing.dart';
 
 class TreeListScreen extends StatefulWidget {
   const TreeListScreen({Key? key}) : super(key: key);
@@ -15,13 +18,6 @@ class TreeListScreen extends StatefulWidget {
 class _TreeListScreenState extends State<TreeListScreen> {
   final TreeListViewModelBase viewModel =
       DependecyInjection.instance.get<TreeListViewModelBase>();
-  final Decoration bodyTextDecoration = const BoxDecoration(
-      borderRadius: BorderRadius.all(Radius.circular(20)),
-      color: Colors.white70);
-
-  final Decoration bodyDecoration = const BoxDecoration(
-      image: DecorationImage(
-          image: AssetImage('assets/beach.jpg'), fit: BoxFit.cover));
 
   @override
   void initState() {
@@ -41,20 +37,39 @@ class _TreeListScreenState extends State<TreeListScreen> {
       appBar: AppBar(title: const Text('Globo Fitness')),
       drawer: const MenuDrawer(),
       bottomNavigationBar: const MenuBottom(),
-      body: Container(
-        decoration: bodyDecoration,
-        child: Center(
-          child: Observer(builder: (context) => ListView(children: _items())),
-        ),
-      ),
+      body: Observer(builder: observerBuilder),
     );
   }
 
-  List<ListTile> _items() {
-    List<ListTile> tiles = [];
-    for (var item in viewModel.trees) {
-      tiles.add(ListTile(title: Text(item.name ?? "")));
-    }
-    return tiles;
+  Widget observerBuilder(BuildContext context) => viewModel.trees.isEmpty
+      ? getActivityIndicator()
+      : ListView.separated(
+          itemCount: viewModel.trees.length,
+          itemBuilder: itemBuilder,
+          separatorBuilder: separatorBuilder,
+          shrinkWrap: true);
+
+  Widget itemBuilder(BuildContext context, int index) {
+    final Tree tree = viewModel.trees[index];
+    return ListTile(
+      textColor: Colors.black,
+      key: Key(tree.id.toString()),
+      title: Text(tree.name ?? ""),
+      subtitle: getSubtitle(tree.species, index),
+      trailing: const Icon(Icons.arrow_right_outlined),
+      // onTap: () => goToDetailTree(context, tree),
+    );
   }
+
+  Text getSubtitle(String? subtitle, int index) => Text(subtitle != null
+      ? 'Species : $subtitle'.toTitleCase()
+      : 'Species Not Speficied');
+
+  Padding separatorBuilder(BuildContext context, int index) => const Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Divider(
+          color: Colors.grey,
+          indent: 10,
+        ),
+      );
 }
---
+++ lib/shared/circular_progress_indicator.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Widget getActivityIndicator() =>
+    const Center(child: CircularProgressIndicator(color: Colors.black));
