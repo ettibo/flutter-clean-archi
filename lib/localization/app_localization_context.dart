c07e1b49f776c613aba8672df6e8ea7da3faaf2a
Dylan Hamiti <d.hamiti@groupeonepoint.com>
2022-08-10 14:32:15 +0200

    Configured and added the files needed for the localization

---
+++ lib/localization/app_localization_context.dart
@@ -0,0 +1,7 @@
+// app_localizations_context.dart
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+extension LocalizedBuildContext on BuildContext {
+  AppLocalizations get localized => AppLocalizations.of(this);
+}
