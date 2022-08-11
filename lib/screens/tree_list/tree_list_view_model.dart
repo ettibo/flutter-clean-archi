a13da821c6419447921040faf519b80b96f6584c
Dylan Hamiti <dylanhamiti@gmail.com>
2022-08-11 16:57:38 +0200

    Put the logic of getting the title and subtitle of a TreeListItem in the ViewModel

--- lib/screens/tree_list/tree_list_view_model.dart
+++ lib/screens/tree_list/tree_list_view_model.dart
@@ -1,3 +1,5 @@
+import 'package:globo_fitness/localization/app_localization_context.dart';
+import 'package:globo_fitness/extensions/string_casing.dart';
 import 'package:api/dependency_injection.dart';
 import 'package:api/models/app/tree/tree.dart';
 import 'package:api/use_case/tree/tree_list.dart';
@@ -25,4 +27,11 @@ abstract class TreeListViewModelBase with Store, ViewModel {
     List<Tree> newTrees = await useCase.fetch(startRow: 0, nbRows: 50);
     trees.addAll(newTrees);
   }
+
+  //UI Methods
+  Text getTitle(BuildContext context, String? treeName) =>
+      Text(treeName ?? context.localized.treeWithoutName);
+
+  Text getSubtitle(BuildContext context, String? subtitle, int index) => Text(
+      '${context.localized.species} : ${subtitle != null ? subtitle.toTitleCase() : context.localized.speciesNotSpecified}');
 }
