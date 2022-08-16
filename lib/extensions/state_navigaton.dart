da748a0a3b03e6e85fd8bc690374f2fc9d0e73ed
Thibaud Huchon <thibaud.huchon@gmail.com>
2022-08-16 17:52:31 +0200

    renaming of var

--- lib/extensions/state_navigaton.dart
+++ lib/extensions/state_navigaton.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 extension StateNavigation on State {
-  void navigateTo(Widget widget) {
+  void navigateTo(Widget screen) {
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => widget,
+      builder: (context) => screen,
     ));
   }
 }
