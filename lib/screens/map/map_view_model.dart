664b08c6bab9502ed7daeca0eddf6d050a90ab0d
Dylan Hamiti <dylanhamiti@gmail.com>
2022-08-19 10:56:43 +0200

    Added Clustering in the Map with custom Marker

--- lib/screens/map/map_view_model.dart
+++ lib/screens/map/map_view_model.dart
@@ -1,20 +1,17 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
-import 'package:globo_fitness/store/tree_store.dart';
-import 'package:globo_fitness/template/view_model/view_model.dart';
+import 'package:latlong2/latlong.dart' show LatLng;
+import 'package:flutter_map/flutter_map.dart';
 
 import 'package:api/dependency_injection.dart';
 
-import 'package:api/models/app/tree/tree.dart';
+import 'package:globo_fitness/screens/map/tree_marker.dart';
 
+import 'package:globo_fitness/store/tree_store.dart';
+
+import 'package:globo_fitness/template/view_model/view_model.dart';
 import 'package:globo_fitness/extensions/nullable_check.dart';
-
-// import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
-import 'package:flutter_map/flutter_map.dart';
-
-import 'package:latlong2/latlong.dart' show LatLng;
 
 part 'map_view_model.g.dart';
 
@@ -24,21 +21,20 @@ abstract class MapViewModelBase with Store, ViewModel {
   final TreeStoreBase treeStore =
       DependecyInjection.instance.get<TreeStoreBase>();
 
-  // final PopupController popupController = PopupController();
-
   final LatLng parisCoord = LatLng(48.866667, 2.333333);
-
   final double defaultZoom = 13.0;
   final double maxZoom = 18.0;
-
   final int heightMarker = 50;
   final int widthMarker = 50;
+  final int maxClusterRadius = 120;
+  final Size clusterSize = const Size(40, 40);
 
   @observable
   ObservableList<Marker> treesMarkers = ObservableList();
 
   @override
   void init() {
+    clearMarkerList();
     generateMarkers();
   }
 
@@ -53,14 +49,9 @@ abstract class MapViewModelBase with Store, ViewModel {
         tree.lng.let((that) {
           newMarkers.add(Marker(
               point: LatLng(tree.lat!, tree.lng!),
-              builder: (context) => marker()));
+              builder: (context) => treeMarker()));
         });
       });
-      // if (tree.lat != null && tree.lng != null) {
-      //   newMarkers.add(Marker(
-      //       point: LatLng(tree.lat!, tree.lng!),
-      //       builder: (context) => marker()));
-      // }
     }
 
     treesMarkers.addAll(newMarkers);
@@ -68,6 +59,5 @@ abstract class MapViewModelBase with Store, ViewModel {
 
   void clearMarkerList() => treesMarkers.clear();
 
-  Widget marker() => ElevatedButton(
-      onPressed: () => print("fzefez"), child: const Text('Marker'));
+  Widget treeMarker() => const TreeMarker();
 }
