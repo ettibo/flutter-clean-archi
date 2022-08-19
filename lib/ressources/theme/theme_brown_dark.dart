bd25f97d5f332bb5530c98bef885e32bf1aea25f
DimitriPocheron <d.pocheron@groupeonepoint.com>
2022-08-19 12:19:48 +0200

    Begin to create and configure AppConfig

---
+++ lib/ressources/theme/theme_brown_dark.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData brownDarkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primarySwatch: Colors.brown,
+  primaryColor: Colors.white,
+  backgroundColor: const Color(0xff333333),
+  primaryColorDark: Colors.brown,
+  primaryColorLight: Colors.black,
+  bottomAppBarColor: Colors.brown,
+  indicatorColor: Colors.brown,
+);
---
+++ lib/ressources/theme/theme_brown_light.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData brownLightTheme = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.blue,
+  primaryColor: Colors.brown,
+  backgroundColor: Colors.white,
+  primaryColorDark: Colors.brown,
+  primaryColorLight: Colors.black,
+  bottomAppBarColor: Colors.brown,
+  indicatorColor: Colors.brown,
+);
