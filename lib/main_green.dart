d932c54a73a3c7793964c7f9770e77024f1a64f4
DimitriPocheron <d.pocheron@groupeonepoint.com>
2022-08-30 09:03:28 +0200

    Update main_green.dart

--- lib/main_green.dart
+++ lib/main_green.dart
@@ -1,14 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:globo_fitness/app_config.dart';
 import 'package:globo_fitness/main_common.dart';
 
 import 'ressources/theme/green/theme_green_light.dart';
 import 'ressources/theme/green/theme_green_dark.dart';
 
-void main() {
+void main() async {
   final appConfig = AppConfig(
       appTitle: 'Les Arbres Verts',
       lightTheme: greenLightTheme,
       darkTheme: greenDarkTheme);
-
-  mainCommon(appConfig: appConfig);
+  Widget app = await initializeApp(appConfig);
+  runApp(app);
 }
