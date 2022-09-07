df540148014b4910c9ce4c85229b43b6d5e783dc
Dylan Hamiti <dylanhamiti@gmail.com>
2022-09-07 17:22:49 +0200

    Added 'platform specific widgets' library & new wrapper for specific widgets

---
+++ lib/shared/platform_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+PlatformAppBar platformAppBar(
+        {required BuildContext context,
+        Widget? leading,
+        String? title,
+        List<Widget>? trailingWidgets,
+        Color? backgroundColor}) =>
+    PlatformAppBar(
+      title: Text(title ?? '', style: Theme.of(context).textTheme.titleMedium),
+      leading: leading,
+      trailingActions: trailingWidgets,
+      backgroundColor: backgroundColor ?? Theme.of(context).backgroundColor,
+      material: (context, _) => MaterialAppBarData(
+        iconTheme: IconThemeData(color: Theme.of(context).secondaryHeaderColor),
+      ),
+    );
