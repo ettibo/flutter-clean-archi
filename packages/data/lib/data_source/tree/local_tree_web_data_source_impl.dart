3c7b301a2929ba08f9fe566f202db84b868ae799
DimitriPocheron <d.pocheron@groupeonepoint.com>
2022-09-07 07:44:07 +0200

    Add LocalWebTree && LocalTreeWebDataSourceImpl

---
+++ packages/data/lib/data_source/tree/local_tree_web_data_source_impl.dart
@@ -0,0 +1,19 @@
+import 'package:api/data_source/tree/local_tree_data_source.dart';
+import 'package:api/models/app/tree/tree.dart';
+
+class LocalTreeWebDataSourceImpl implements LocalTreeDataSource {
+  @override
+  int clear() {
+    return 0;
+  }
+
+  @override
+  List<Tree> getTreeList() {
+    return [];
+  }
+
+  @override
+  List<int> saveTreeList(List<Tree> list) {
+    return [];
+  }
+}
--- packages/data/lib/models/local/tree/local_tree.dart
+++ packages/data/lib/models/local/tree/local_tree.dart
@@ -1,20 +1,19 @@
-// import 'package:data/models/local/base_local/base_local_object.dart'
-//     if (dart.library.html) '';
-// import 'package:objectbox/objectbox.dart' if (dart.library.html) '';
+import 'package:data/models/local/base_local/base_local_object.dart';
+import 'package:objectbox/objectbox.dart';
 
-// @Entity()
-// class LocalTree implements BaseLocalObject {
-//   @Id(assignable: true)
-//   int? id;
-//   String? name;
-//   String? address;
-//   String? address2;
-//   String? species;
-//   int? height;
-//   int? circumference;
-//   double? lat;
-//   double? lng;
+@Entity()
+class LocalTree implements BaseLocalObject {
+  @Id(assignable: true)
+  int? id;
+  String? name;
+  String? address;
+  String? address2;
+  String? species;
+  int? height;
+  int? circumference;
+  double? lat;
+  double? lng;
 
-//   @override
-//   int getId() => id ?? 0;
-// }
+  @override
+  int getId() => id ?? 0;
+}
