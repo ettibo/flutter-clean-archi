86b3682331b03512ada9ff95ea77c8d848e5ea27
ZMontassar <m.zarroug@groupeonepoint.com>
2022-09-08 17:51:18 +0200

    first commit: Check internet connection and test it on the List of Tree

--- lib/injection/managers_injection.dart
+++ lib/injection/managers_injection.dart
@@ -1,3 +1,4 @@
+import 'package:api/models/app/managers/connection_status.dart';
 import 'package:flutter/foundation.dart';
 import 'package:api/dependency_injection.dart';
 import 'package:api/models/app/managers/remote_config.dart';
@@ -5,6 +6,7 @@ import 'package:api/models/app/managers/crash.dart';
 
 import 'package:data/firebase/remote_config/firebase_remote_config.dart';
 import 'package:data/firebase/crashlytics/firebase_crashlytics.dart';
+import 'package:globo_fitness/managers/connection_check_status.dart';
 
 Future<void> registerManagers() async {
   DependecyInjection.instance
@@ -12,4 +14,5 @@ Future<void> registerManagers() async {
   if (!kIsWeb) {
     DependecyInjection.instance.inject<CrashManager>(CrashlyticsFirebase());
   }
+  DependecyInjection.instance.inject<ConnectionStatus>(ConnectionCheckStatus());
 }
