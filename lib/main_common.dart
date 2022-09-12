b8fd0611fd69a2a49b297baf8a9b2a8a5f7d2ed1
ZMontassar <m.zarroug@groupeonepoint.com>
2022-09-12 09:50:00 +0200

    Commit: Fix a feed-backs and add some modifications in the code.

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,11 +1,14 @@
-import 'package:api/dependency_injection.dart';
-import 'package:api/models/app/managers/connection_status.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:api/dependency_injection.dart';
+import 'package:api/models/app/managers/connection_status.dart';
+
 import 'package:adaptive_theme/adaptive_theme.dart';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:globo_fitness/firebase/green_firebase_options.dart'
     as green_firebase_options;
 import 'package:globo_fitness/firebase/brown_firebase_options.dart'
@@ -15,13 +18,11 @@ import 'package:globo_fitness/screens/home/home_screen.dart';
 
 import 'package:globo_fitness/managers/remote_config_manager.dart';
 import 'package:globo_fitness/managers/crash_manager.dart';
-
 import 'package:globo_fitness/injection/app_injection.dart';
+import 'package:globo_fitness/translations/codegen_loader.g.dart';
 
 import 'package:globo_fitness/app_config.dart';
 import 'package:easy_localization/easy_localization.dart';
-
-import 'package:globo_fitness/translations/codegen_loader.g.dart';
 
 Future<Widget> initializeApp(AppConfig appConfig) async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -89,27 +90,30 @@ Future<void> _firebaseInitializer(FlavorName flavorName) async {
   await Firebase.initializeApp(options: firebaseOption);
 }
 
-bool hasInterNetConnection = false;
 
 void _activateManagers() {
-  final ConnectionStatus connectionStatus =
-      DependecyInjection.instance.get<ConnectionStatus>();
-
-  connectionStatus.initialize();
-  //Listen for connection change
-  connectionStatus.connectionChange
-      .asBroadcastStream()
-      .listen(connectionChanged);
-      debugPrint(connectionChanged.toString());
+  getConnectionStatus();
 
   if (!kIsWeb) {
     activateCrashReportIfEnabled();
   }
 }
 
+void getConnectionStatus() {
+  final ConnectionStatus connectionStatus =
+      DependecyInjection.instance.get<ConnectionStatus>();
+
+  connectionStatus.initialize();
+  //Listen for connection change
+  connectionStatus.connectionChangeStream
+      .asBroadcastStream()
+      .listen(connectionChanged);
+}
+
 void connectionChanged(dynamic hasConnection) {
+  bool hasInterNetConnection = false;
   hasInterNetConnection = hasConnection;
-  if(hasInterNetConnection){
-        activateRemoteConfig();
-  } 
+  if (hasInterNetConnection) {
+    activateRemoteConfig();
+  }
 }
