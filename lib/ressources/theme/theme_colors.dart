49c93ac880c2613bbe1d1b504f960f405bfe9248
Dylan Hamiti <dylanhamiti@gmail.com>
2022-09-14 14:30:35 +0200

    FIxed a color name in the "ThemeColor" file

--- lib/ressources/theme/theme_colors.dart
+++ lib/ressources/theme/theme_colors.dart
@@ -6,6 +6,5 @@ class ThemeColors {
   static const Color darkBrown = Color(0xffC4A484);
   static const Color lightBrown = Color(0xff654321);
 
-  static const Color darkGreen = Color(0xff65793C);
-  static const Color lightGreen = Color(0xff65793C);
+  static const Color green = Color(0xff65793C);
 }
