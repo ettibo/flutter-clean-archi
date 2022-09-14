7e8f7acca1e6346244cb5287e8590af644765f8c
Dylan Hamiti <dylanhamiti@gmail.com>
2022-09-14 17:21:32 +0200

    Refactor "TreeDetailScreen" to Stateless Widget

--- lib/screens/tree_detail/tree_detail_screen.dart
+++ lib/screens/tree_detail/tree_detail_screen.dart
@@ -10,19 +10,14 @@ import 'package:globo_fitness/shared/platform_app_bar.dart';
 import 'package:globo_fitness/translations/locale_keys.g.dart';
 import 'package:globo_fitness/extensions/string_localized.dart';
 
-class TreeDetailScreen extends StatefulWidget {
+class TreeDetailScreen extends StatelessWidget {
   final Tree tree;
   const TreeDetailScreen({Key? key, required this.tree}) : super(key: key);
 
-  @override
-  State<TreeDetailScreen> createState() => _TreeDetailScreenState();
-}
-
-class _TreeDetailScreenState extends State<TreeDetailScreen> {
   PlatformAppBar appBar(BuildContext context) => platformAppBar(
         context: context,
-        title: widget.tree.name ??
-            LocaleKeys.title_title_detail_tree_screen.localized(),
+        title:
+            tree.name ?? LocaleKeys.title_title_detail_tree_screen.localized(),
       );
 
   @override
@@ -37,7 +32,7 @@ class _TreeDetailScreenState extends State<TreeDetailScreen> {
             rowTreeInfo(
                 context: context,
                 title: LocaleKeys.tree_list_screen_species.localized(),
-                value: widget.tree.species ??
+                value: tree.species ??
                     LocaleKeys.tree_list_screen_species_not_specified
                         .localized()),
             rowTreeInfo(
@@ -45,19 +40,19 @@ class _TreeDetailScreenState extends State<TreeDetailScreen> {
               title: LocaleKeys.tree_detail_screen_address.localized(),
               value: LocaleKeys.tree_detail_screen_value_with_unit.localized(
                   args: {
-                    'value': widget.tree.address2.toString(),
-                    'unit': widget.tree.address.toString()
+                    'value': tree.address2.toString(),
+                    'unit': tree.address.toString()
                   }),
             ),
             rowTreeInfo(
                 context: context,
                 title: LocaleKeys.tree_detail_screen_height.localized(),
-                value: widget.tree.height.toString(),
+                value: tree.height.toString(),
                 unit: LocaleKeys.tree_detail_screen_meter_unit.localized()),
             rowTreeInfo(
                 context: context,
                 title: LocaleKeys.tree_detail_screen_circumference.localized(),
-                value: widget.tree.circumference.toString(),
+                value: tree.circumference.toString(),
                 unit:
                     LocaleKeys.tree_detail_screen_centimeter_unit.localized()),
           ],
