501de1cd3ecda159286cf1d25633a612589e4ea1
Dylan Hamiti <dylanhamiti@gmail.com>
2022-09-16 17:59:26 +0200

    Deleted some unnecessary observables and clean some code

--- lib/screens/map/map_view_model.g.dart
+++ lib/screens/map/map_view_model.g.dart
@@ -9,108 +9,10 @@ part of 'map_view_model.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$MapViewModel on MapViewModelBase, Store {
-  late final _$centerCurrentLocationStreamControllerAtom = Atom(
-      name: 'MapViewModelBase.centerCurrentLocationStreamController',
-      context: context);
-
-  @override
-  StreamController<double?> get centerCurrentLocationStreamController {
-    _$centerCurrentLocationStreamControllerAtom.reportRead();
-    return super.centerCurrentLocationStreamController;
-  }
-
-  @override
-  set centerCurrentLocationStreamController(StreamController<double?> value) {
-    _$centerCurrentLocationStreamControllerAtom
-        .reportWrite(value, super.centerCurrentLocationStreamController, () {
-      super.centerCurrentLocationStreamController = value;
-    });
-  }
-
-  late final _$treesMarkersAtom =
-      Atom(name: 'MapViewModelBase.treesMarkers', context: context);
-
-  @override
-  ObservableList<Marker> get treesMarkers {
-    _$treesMarkersAtom.reportRead();
-    return super.treesMarkers;
-  }
-
-  @override
-  set treesMarkers(ObservableList<Marker> value) {
-    _$treesMarkersAtom.reportWrite(value, super.treesMarkers, () {
-      super.treesMarkers = value;
-    });
-  }
-
-  late final _$centerOnLocationUpdateAtom =
-      Atom(name: 'MapViewModelBase.centerOnLocationUpdate', context: context);
-
-  @override
-  CenterOnLocationUpdate get centerOnLocationUpdate {
-    _$centerOnLocationUpdateAtom.reportRead();
-    return super.centerOnLocationUpdate;
-  }
-
-  @override
-  set centerOnLocationUpdate(CenterOnLocationUpdate value) {
-    _$centerOnLocationUpdateAtom
-        .reportWrite(value, super.centerOnLocationUpdate, () {
-      super.centerOnLocationUpdate = value;
-    });
-  }
-
-  late final _$determinePositionAsyncAction =
-      AsyncAction('MapViewModelBase.determinePosition', context: context);
-
-  @override
-  Future<Position> determinePosition(BuildContext context) {
-    return _$determinePositionAsyncAction
-        .run(() => super.determinePosition(context));
-  }
-
-  late final _$MapViewModelBaseActionController =
-      ActionController(name: 'MapViewModelBase', context: context);
-
-  @override
-  void generateMarkers() {
-    final _$actionInfo = _$MapViewModelBaseActionController.startAction(
-        name: 'MapViewModelBase.generateMarkers');
-    try {
-      return super.generateMarkers();
-    } finally {
-      _$MapViewModelBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
-  @override
-  void _clearMarkerList() {
-    final _$actionInfo = _$MapViewModelBaseActionController.startAction(
-        name: 'MapViewModelBase._clearMarkerList');
-    try {
-      return super._clearMarkerList();
-    } finally {
-      _$MapViewModelBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
-  @override
-  void centerOnUser() {
-    final _$actionInfo = _$MapViewModelBaseActionController.startAction(
-        name: 'MapViewModelBase.centerOnUser');
-    try {
-      return super.centerOnUser();
-    } finally {
-      _$MapViewModelBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
   @override
   String toString() {
     return '''
-centerCurrentLocationStreamController: ${centerCurrentLocationStreamController},
-treesMarkers: ${treesMarkers},
-centerOnLocationUpdate: ${centerOnLocationUpdate}
+
     ''';
   }
 }
