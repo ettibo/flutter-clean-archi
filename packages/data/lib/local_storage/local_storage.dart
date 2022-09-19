c9d2cd4bfff42e93413fd0328ef2c802c4bc5c6a
Dylan Hamiti <dylanhamiti@gmail.com>
2022-09-19 11:45:02 +0200

    Updated "locale_storage" to enable sorting with a specific property of the table

--- packages/data/lib/local_storage/local_storage.dart
+++ packages/data/lib/local_storage/local_storage.dart
@@ -1,9 +1,15 @@
+import 'package:objectbox/internal.dart';
+
 abstract class LocalStorage {
   int addOrUpdateItem<T>(T item);
   List<int> addOrUpdateItems<T>(List<T> items);
   void removeItem<T>(T item);
   void removeItems<T>(List<T> items);
   T? getItem<T>(int id);
-  List<T> getItems<T>();
+  List<T> getItems<T, U>(
+      {int startRow = 0,
+      int nbRows = 20,
+      QueryProperty<T, U>? orderProperty,
+      int orderFlags = 0});
   int clear<T>();
 }
